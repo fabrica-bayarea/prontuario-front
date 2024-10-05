36839d317d4a5aadba77a6dc122d1a19ab526f25
gabrielsb18 <gbmodelator@gmail.com>
2024-10-05 04:13:20 -0300

    refactor: removendo importação

--- src/components/Tables/Admin/TableProgramsAdmin/TableProgramsAdmin.tsx
+++ src/components/Tables/Admin/TableProgramsAdmin/TableProgramsAdmin.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import style from "./style.module.css";
 import Image from "next/image";
-import Modal from "@/components/Modal/modal";
 import HeaderTable from "@/components/HeaderTable/HeaderTable";
 
 interface ProgramaAPI  {
